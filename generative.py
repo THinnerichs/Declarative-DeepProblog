fc5457095ac77eb288f20ab8f9ca4959e00ad4b1
Robin Manhaeve <robin.manhaeve@cs.kuleuven.be>
2023-03-09 16:51:16 +0100

    Generation for addition.

--- generative.py
+++ generative.py
@@ -11,7 +11,7 @@ from deepproblog.logger import VerboseLogger
 from deepproblog.model import Model
 from deepproblog.query import Query
 
-# model = Model.from_file('vae.pl', logger=VerboseLogger(1000))
+model = Model.from_file('vae.pl', logger=VerboseLogger(1000))
 
 #
 # model.fit(batch_size=4, stop_condition=1)
@@ -31,7 +31,6 @@ class LatentSource(Mapping[Term, torch.Tensor]):
     def __getitem__(self, index: tuple[Term]) -> torch.Tensor:
         i = torch.LongTensor([int(index[0])])
         tensor = self.data(i)[0]
-        print(tensor)
         return tensor
 
     def __len__(self) -> int:
@@ -39,31 +38,35 @@ class LatentSource(Mapping[Term, torch.Tensor]):
 
 
 with open('model_vae.dpl', 'rb') as f:
-    model = pickle.load(f)
+    model2 = pickle.load(f)
 
-# model.networks = model2.networks
+model.networks = model2.networks
 model.freeze()
 latent = LatentSource()
 model.add_tensor_source('latent', latent)
 
-optim = torch.optim.Adam(latent.data.parameters(), lr=1e-1)
+optim = torch.optim.Adam(latent.data.parameters(), lr=1e-2)
 # mnist_test = MNIST('mnist_test')
 
 engine = ExactEngine(model)
 
-query = Query(Term('digit', Var('X'), Constant(3)))
+query = Query(Term('addition', Var('X'), Var('Y'), Constant(8)))
 ac = engine.query(query)
 for i in range(1001):
     results = ac.evaluate(model)
-    for key in results:
-        tensor, label = key.args
-        probability = results[key]
-        tensor = model.get_tensor(tensor).detach()
-        loss = -torch.log(probability)
-        if i % 100 == 0:
-            save_image(tensor, '{}_{}.png'.format(i, key), value_range=(-1.0, 1.0))
-            print(key, ':', float(probability))
-            print('Loss: ', loss)
-        optim.zero_grad()
-        loss.backward()
-        optim.step()
+    key = max(results, key = lambda x: results[x])
+    # for key in results:
+    tensor1_term, tensor2_term, label = key.args
+    probability = results[key]
+    tensor1 = model.get_tensor(tensor1_term).detach()
+    tensor2 = model.get_tensor(tensor2_term).detach()
+
+    loss = -torch.log(probability)
+    if i % 200 == 0:
+        save_image(tensor1, '{}_{}.png'.format(tensor1_term, i), value_range=(-1.0, 1.0))
+        save_image(tensor2, '{}_{}.png'.format(tensor2_term, i), value_range=(-1.0, 1.0))
+        print(key, ':', float(probability))
+        print('Loss: ', loss)
+    optim.zero_grad()
+    loss.backward()
+    optim.step()
--- vae.pl
+++ vae.pl
@@ -4,6 +4,9 @@
 :- include_network(encoder, 'vae.py', 'encoder').
 :- include_network(decoder, 'vae.py', 'decoder').
 
+latent(tensor(latent(I))) :- between(0,1,I).
+
+addition(Img1,Img2,Sum) :- digit(Img1,D1), digit(Img2,D2), Sum is D2+D1.
 
 nn(discriminator,[Image],Digit,[0,1,2,3,4,5,6,7,8,9]) :: digit(Image,Digit) :- image(Image).
 
@@ -11,7 +14,7 @@ nn(encoder,[Image], Latent) :: encoder(Image, Latent).
 nn(decoder,[Latent], Image) :: decoder(Latent, Image).
 
 image(Image) :- ground(Image), encoder(Image, Latent),decoder(Latent, Image2), similar(Image, Image2).
-image(Image) :- var(Image), decoder(tensor(latent(0)),Image).
+image(Image) :- var(Image),latent(Latent), decoder(Latent, Image).
 
 similar(X,X).
 P :: similar(Image1, Image2) :- Image1 \= Image2, rbf(Image1, Image2, P).
